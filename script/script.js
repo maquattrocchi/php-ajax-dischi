c0c916ce305cccc91442370bf6971bbf10146c12
maquattrocchi <martinaquattrocchi5@gmail.com>
2022-05-25 16:54:54 +0200

    creazione select mediante chiamata api

--- script/script.js
+++ script/script.js
@@ -2,13 +2,25 @@ const app = new Vue({
     el: '#app',
     data:{
         arrDiscs: [],
+        selectedGenre: '',
+        genres: [],
     },
     methods:{
         getDisc(){
             axios.get('server.php').then((res)=>{
                 this.arrDiscs = res.data;
                 console.log(this.arrDiscs);
+                this.arrDiscs.forEach((disc) => {
+                    if(!this.genres.includes(disc.genre)){
+                        this.genres.push(disc.genre)
+                    }
+                console.log(this.genres)
+                });
             })
+            console.log(this.selectedGenre)
+        },
+        searchGenre(){
+            console.log(this.selectedGenre)
         }
     },
     mounted(){
